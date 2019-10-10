3f0c9ad4475cd8b381fa499603d563b4307cdf5f
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-10-10 21:21:42 +0700

    first commit

---
+++ api/main.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func ShowNama(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Hello, World!")
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/hello", ShowNama).Methods(http.MethodGet)
+
+	router.ServeHTTP(w, r)
+}
